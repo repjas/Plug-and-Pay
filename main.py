b6cd5c02c7344cb520cca5c1cf3fc54f620ad6d2
repjas <52012627+repjas@users.noreply.github.com>
2022-09-12 21:12:00 +0200

    fix

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 if platform.system() == 'Windows':
+    from webdriver_manager.chrome import ChromeDriverManager
     from selenium.webdriver.chrome.service import Service as ChromeService
     from selenium.webdriver.chrome.options import Options
 import json
